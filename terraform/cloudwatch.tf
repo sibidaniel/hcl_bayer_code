9071ad0f67b9b9ffeaf7828881b2ca2811123e37
sibi daniel <sibidaniel95@gmail.com>
2025-05-30 14:52:31 +0530

    [WIP] - [FIRST-01] - update workflow

--- terraform/cloudwatch.tf
+++ terraform/cloudwatch.tf
@@ -11,9 +11,3 @@ resource "aws_cloudwatch_log_stream" "lambda_log_stream" {
   name           = "my_lambda_function_stream"
   log_group_name = aws_cloudwatch_log_group.lambda_log_group.name
 }
-resource "aws_cloudwatch_log_subscription_filter" "lambda_subscription" {
-  name            = "lambda_subscription_filter"
-  log_group_name  = aws_cloudwatch_log_group.lambda_log_group.name
-  filter_pattern  = ""
-  destination_arn = aws_lambda_function.my_lambda_function.arn
-}
